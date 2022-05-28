078976e13391efedadd7e9176dc5023f9219fba1
Tomasz Czmyr <tomasz.czmyr@bilansjr.pl>
2022-05-28 14:51:10 +0200

    Init

---
+++ src/test/java/pl/javascratches/jwtrestapi/JwtRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javascratches.jwtrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
